075017fdd374fa09a1259a7c024d9267845b6b83
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-04-06 13:56:49 -0400

    replace guidelines with about accordion, make entire about page CMS driven

--- src/api/guideline/controllers/guideline.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  guideline controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::guideline.guideline');
